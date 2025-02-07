ecad1962424704fc3efd96943bcbd84df7754f66
Isaac-Jimenez-1347 <164533988+Isaac-Jimenez-1347@users.noreply.github.com>
2025-02-07 16:47:39 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,18 @@
+//funcion que no devuelve el valor 
+// funcion saludar 
+void saludar(){
+  print('hola mundo');
+}
+void escribeNombre(){
+  print('Isaac Jimenez');
+}
+
+  void main(){
+    // Isaac Jimenez Mat: 22308051281347
+    // llamar a la funcion saludar 
+    print('Isaac Jimenez Mat: 22308051281347');
+    print('llamando a la funcion saludar');
+    saludar();
+    print('llamando a la funcion escribeNombre');
+    escribeNombre();
+  } 
