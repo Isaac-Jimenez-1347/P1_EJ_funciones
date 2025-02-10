055dc46b43b94ffe97043691168fbe4ec2440dca
Isaac-Jimenez-1347 <164533988+Isaac-Jimenez-1347@users.noreply.github.com>
2025-02-10 18:32:30 +0000

    publique funcion flecha

---
+++ funcionFlecha.dart
@@ -0,0 +1,23 @@
+int multiplicacion(a,b)=> a*b;
+
+int multiplicacionn(a,b){
+  return a*b;
+}
+
+double divide(a,b)=> a/b;
+
+double divicion(a,b){
+  return a/b;
+}
+void main(){
+  print('Isaac Jimenez Hdz');
+  print('llamando a la funcion multiplicacion');
+  print(multiplicacion(10,5));
+  print('llamando a la funcion multiplicacionn');
+  print(multiplicacionn(10,2));
+  print('llamando a la funcion divide');
+  print(divide(10,3));
+  print('llamando a la funcion divicion');
+  print(divicion(10,3));
+
+}
--- recursos/funcionsuma.md
+++ recursos/funcionsuma.md
@@ -1,2 +1,7 @@
 Ejemplo salida 
+
 ![alt text](image.png)
+
+Ejemplo de salida funcion normal + funcion con flecha
+
+![alt text](image-1.png)
