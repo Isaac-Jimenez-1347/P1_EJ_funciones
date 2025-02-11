9d0184ec9c35a303f74b88129d0c0cb88dbc66da
Isaac-Jimenez-1347 <164533988+Isaac-Jimenez-1347@users.noreply.github.com>
2025-02-11 20:39:12 +0000

    problema1.dart

--- problema1.dart
+++ problema1.dart
@@ -1,11 +1,23 @@
-// Función para obtener la lista de números
-List<int> obtenerLista() {
-  return [1, 2, 3, 4, 5]; // Lista predefinida
+import 'dart:io';
+
+// Función para capturar los datos de la lista
+List<int> capturarDatos() {
+  List<int> numeros = [];
+  print("¿Cuántos números deseas ingresar?");
+  int cantidad = int.parse(stdin.readLineSync()!);
+
+  for (int i = 0; i < cantidad; i++) {
+    print("Ingresa el número ${i + 1}:");
+    int numero = int.parse(stdin.readLineSync()!);
+    numeros.add(numero);
+  }
+
+  return numeros;
 }
 
 // Función para mostrar los elementos y calcular la suma
 void mostrarYCalcular(List<int> numeros) {
-  print("Elementos de la lista:");
+  print("\nElementos de la lista:");
   int suma = 0;
 
   for (int numero in numeros) {
@@ -17,10 +29,10 @@ void mostrarYCalcular(List<int> numeros) {
 }
 
 void main() {
-  // Obtener la lista de números 
+  // Capturar datos de la lista
   print('Isaac Jimenez Hernandez Mat:22308051281347');
-  print('Escribe un programa que tome una lista de números enteros (int) y calcule la suma de todos sus elementos.');
-  List<int> numeros = obtenerLista();
+  print('un programa que tome una lista de números enteros (int) y calcule la suma de todos sus elementos.');
+  List<int> numeros = capturarDatos();
 
   // Mostrar los elementos y calcular la suma
   mostrarYCalcular(numeros);
--- recursos/ejercicio1.md
+++ recursos/ejercicio1.md
@@ -1,4 +1,4 @@
 *Salida de ejercicio #1*
 *Escribe un programa que tome una lista de números enteros (int) y calcule la suma de todos sus elementos.*
 
-![alt text](image-6.png)
+![alt text](image-7.png)
