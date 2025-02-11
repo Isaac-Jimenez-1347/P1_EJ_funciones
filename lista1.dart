25a1556fb42c8d9b44d8a9ae3e3004635fd5c5fb
Isaac-Jimenez-1347 <164533988+Isaac-Jimenez-1347@users.noreply.github.com>
2025-02-11 20:21:24 +0000

    problema 1

---
+++ lista1.dart
@@ -0,0 +1,26 @@
+// Función para obtener la lista de números
+List<int> obtenerLista() {
+  return [1, 2, 3, 4, 5]; // Lista predefinida
+}
+
+// Función para mostrar los elementos y calcular la suma
+void mostrarYCalcular(List<int> numeros) {
+  print("Elementos de la lista:");
+  int suma = 0;
+
+  for (int numero in numeros) {
+    print(numero);
+    suma += numero;
+  }
+
+  print("\nLa suma de los elementos es: $suma");
+}
+
+void main() {
+  // Obtener la lista de números 
+  print('Isaac Jimenez Hernandez Mat:22308051281347');
+  List<int> numeros = obtenerLista();
+
+  // Mostrar los elementos y calcular la suma
+  mostrarYCalcular(numeros);
+}
---
+++ recursos/ejercicio1.md
@@ -0,0 +1,4 @@
+*Salida de ejercicio #1*
+*Escribe un programa que tome una lista de números enteros (int) y calcule la suma de todos sus elementos.*
+
+![alt text](image-5.png)
